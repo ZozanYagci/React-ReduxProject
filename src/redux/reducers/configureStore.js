11ac6ae998c55c615c59c163f46d92b9b0e96778
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2024-08-15 00:00:11 +0300

    Redux Thunk - list of categories

--- src/redux/reducers/configureStore.js
+++ src/redux/reducers/configureStore.js
@@ -1,8 +1,14 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore} from "@reduxjs/toolkit"
 import rootReducer from "./index"
+import {thunk} from "redux-thunk"
+
 
 export default function createConfigureStore(){
  return configureStore({
-    reducer:rootReducer
+    reducer:rootReducer,
+    middleware: (getDefaultMiddleware) => 
+      getDefaultMiddleware().concat(thunk), 
  })
 }
+
+
